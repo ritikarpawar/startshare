ee5df642cc3dd54e0ee4c0a6f297a5c3e008c2b3
ritikarpawar <rajeshpawarritika@gmail.com>
2026-07-27 23:12:24 +0530

    fix backend deploy

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const app = require('./src/app')
 const connectDB = require("./src/db/db")
 
 connectDB()
-app.listen("3000",()=>{
-    console.log("Server is running fine on port 3000")
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running fine on port ${PORT}`)
 })
